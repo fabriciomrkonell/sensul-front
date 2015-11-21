9d5b47aade36c642e784de466cbb19af20e42f05
Fabrício Ronchi <fabricioronchii@gmail.com>
2015-11-21 19:29:32 -0200

    News pages

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var express = require('express'),
 
 router.get('/', function(req, res, next) {
   res.render('layouts/default', {
-  	title: 'Index',
+  	title: 'Home',
   	page: '../index.html',
   	requiresJS: ['app/controllers/index']
   });
@@ -16,6 +16,30 @@ router.get('/realtime', function(req, res, next) {
   	title: 'Tempo real',
   	page: '../realtime.html',
   	requiresJS: ['app/controllers/realtime', 'app/libraries/bootstraptreeview/js/bootstraptreeview.min']
+  });
+});
+
+router.get('/transductor', function(req, res, next) {
+  res.render('layouts/default', {
+    title: 'Transductor',
+    page: '../transductor.html',
+    requiresJS: ['app/controllers/transductor']
+  });
+});
+
+router.get('/users', function(req, res, next) {
+  res.render('layouts/default', {
+    title: 'Usuários',
+    page: '../users.html',
+    requiresJS: ['app/controllers/users']
+  });
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('layouts/default', {
+    title: 'Perfil',
+    page: '../profile.html',
+    requiresJS: ['app/controllers/profile']
   });
 });
 
